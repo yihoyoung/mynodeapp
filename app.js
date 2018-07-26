78426803f80a6cb76316cfb0e115c8f14a7a568c
yihoyoung <yihoyoung@nate.com>
2018-07-26 15:25:15 +0800

    v7

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const Koa = require('koa')
 const app = new Koa()
 
 const route = require('koa-route')
+const path = require('path')
+const serve = require('koa-static')
+
+const _static = serve(path.join(__dirname))
+app.use(_static)
 
 const about = ctx => {
   ctx.response.type = 'html'
