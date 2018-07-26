e0424f0814f8433bfe6d1b75dd2bd447e1d294a1
yihoyoung <yihoyoung@nate.com>
2018-07-26 16:07:15 +0800

    v9

--- app.js
+++ app.js
@@ -13,6 +13,23 @@ const serve = require('koa-static')
 const _static = serve(path.join(__dirname))
 app.use(_static)
 
+const logger = (ctx, next) => {
+  let start = Date.now()
+  next()
+  let end = new Date()
+  let duration = end.getTime() - start
+  let d_s = `${end.getFullYear()}-` +
+    `${end.getMonth() + 1 < 10 ? '0' + (end.getMonth() + 1): end.getMonth() + 1}` +
+    `-${end.getDate()} ` +
+    `${end.getHours() < 10 ? '0' + end.getHours() : end.getHours()}:` +
+    `${end.getMinutes() < 10 ? '0' + end.getMinutes(): end.getMinutes()}:` +
+    `${end.getSeconds() < 10 ? '0' + end.getSeconds(): end.getSeconds()}`
+  console.log(`[${d_s}]: ${ctx.request.method} ${ctx.request.url}` +
+    ` - ${ctx.response.status} - ${duration}ms`)
+}
+
+app.use(logger)
+
 const redirect = ctx => {
   ctx.response.redirect('/')
   ctx.response.body = `<a href="/">Index Page</a>`
@@ -30,5 +47,7 @@ const main = ctx => {
 
 app.use(route.get('/about', about))
 app.use(route.get('/', main))
+
+
 
 app.listen(3000)
