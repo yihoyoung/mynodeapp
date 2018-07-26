e4b75887e86543c8df650c95259696bbb26f2eb6
yihoyoung <yihoyoung@nate.com>
2018-07-26 14:39:08 +0800

    v4

--- app.js
+++ app.js
@@ -7,7 +7,19 @@ const Koa = require('koa')
 const app = new Koa()
 
 const body = ctx => {
-  ctx.response.body = 'Hello, World!'
+  if (ctx.request.accepts('xml')) {
+    ctx.response.type = 'xml'
+    ctx.response.body = '<data>Hello, World!</data>'
+  } else if (ctx.request.accepts('json')) {
+    ctx.response.type = 'json'
+    ctx.response.body = {data: 'Hello, world!'}
+  } else if (ctx.request.accepts('html')) {
+    ctx.response.type = 'html'
+    ctx.response.body = '<p>Hello, World!</p>'
+  } else {
+    ctx.response.type = 'text'
+    ctx.response.body = 'Hello, World!'
+  }
 }
 
 app.use(body)
