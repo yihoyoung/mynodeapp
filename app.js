15567d66a8ea46b6068410305125260e4786ebed
yihoyoung <yihoyoung@nate.com>
2018-07-26 16:42:51 +0800

    v10

--- app.js
+++ app.js
@@ -13,17 +13,17 @@ const serve = require('koa-static')
 const _static = serve(path.join(__dirname))
 app.use(_static)
 
-const logger = (ctx, next) => {
+const logger = async (ctx, next) => {
   let start = Date.now()
-  next()
+  await next()
   let end = new Date()
   let duration = end.getTime() - start
   let d_s = `${end.getFullYear()}-` +
     `${end.getMonth() + 1 < 10 ? '0' + (end.getMonth() + 1): end.getMonth() + 1}` +
     `-${end.getDate()} ` +
     `${end.getHours() < 10 ? '0' + end.getHours() : end.getHours()}:` +
-    `${end.getMinutes() < 10 ? '0' + end.getMinutes(): end.getMinutes()}:` +
-    `${end.getSeconds() < 10 ? '0' + end.getSeconds(): end.getSeconds()}`
+    `${end.getMinutes() < 10 ? '0' + end.getMinutes() : end.getMinutes()}:` +
+    `${end.getSeconds() < 10 ? '0' + end.getSeconds() : end.getSeconds()}`
   console.log(`[${d_s}]: ${ctx.request.method} ${ctx.request.url}` +
     ` - ${ctx.response.status} - ${duration}ms`)
 }
@@ -42,8 +42,23 @@ const about = ctx => {
   ctx.response.body = `<a href='/'>Go to hello wolrd</a>`
 }
 const main = ctx => {
-  ctx.response.body = 'Hello, world!'
+  ctx.throw(500)
+  // ctx.response.body = 'Hello, World!'
 }
+
+const handler = async (ctx, next) => {
+  try {
+    await next()
+  } catch (error) {
+    console.log(error.stack)
+    ctx.response.status = error.statusCode || err.status || 500
+    ctx.response.body = {
+      message: error.message
+    }
+  }
+}
+
+app.use(handler)
 
 app.use(route.get('/about', about))
 app.use(route.get('/', main))
